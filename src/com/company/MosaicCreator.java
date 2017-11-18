ee3310d008b81c74928a35ca98fa0990ad18dd0f
Connor DeYoung <deyoungconnor@gmail.com>
2017-11-17 19:56:45 -0600

    fixed issue where result url would sometimes read as jpg and sometimes would be unknown

--- src/com/company/MosaicCreator.java
+++ src/com/company/MosaicCreator.java
@@ -21,24 +21,26 @@ public class MosaicCreator extends PApplet{
     }
 
     private PImage img;
-    //List of the Images' url
-    private List<String> resultUrls = new ArrayList<>();
+
 
     public void settings() {
-        //Have to find all the images fist to make the Mosaic
+        //Have to find all the images first to make the Mosaic
 
 //        Scanner input = new Scanner(System.in);
 //        System.out.println("Search for an image: ");
         String search = "kittens";
 
         String userAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
-        //TODO Change the whatever they searched
+        //TODO Change the image to whatever they searched
         String searchURL = "https://www.google.com/search?site=imghp&tbm=isch&source=hp&q=" + search + "&gws_rd=cr";
 
         try {
             Document doc = Jsoup.connect(searchURL).userAgent(userAgent).referrer("https://www.google.com/").get();
 
             Elements elements = doc.select("div.rg_meta");
+
+            //List of the Images' url
+            List<String> resultUrls = new ArrayList<>();
 
             //Go through the JSON we got back and get the URLs
             JSONObject jsonObject;
@@ -56,21 +58,33 @@ public class MosaicCreator extends PApplet{
                 System.out.println(i + ": " + imageUrl);
                 i++;
             }
+
+            String url = resultUrls.get(0);
+            //Sometimes the image url includes .jpg at the end and sometimes it doesn't.
+            //The loadImage method needs to know the file extension and if the url it doesn't include it,
+            // it needs to know to add it.
+            if(url.substring(url.length()-4, url.length()).equals(".jpg")){
+                img = loadImage(url);
+            }else{
+                img = loadImage(url, "jpg");
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        size(600, 749);
+        size(img.width, img.height);
     }
 
     public void setup() {
-        String url = resultUrls.get(0);
-        img = loadImage(url);
+
 
     }
 
     public void draw() {
         image(img, 0, 0);
 
+
+        noLoop();
     }
 }
