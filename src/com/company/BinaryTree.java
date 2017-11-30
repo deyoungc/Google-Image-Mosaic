b264c24c3aeb78284d31564c27f3b80dda1206ad
Connor DeYoung <deyoungconnor@gmail.com>
2017-11-29 18:04:16 -0600

    added BST for brightness values

--- src/com/company/BinaryTree.java
+++ src/com/company/BinaryTree.java
@@ -7,9 +7,49 @@ public class BinaryTree {
         this.root = null;
     }
 
-    public Node Insert(int value) {
-
-        return new Node(2);
+    public boolean find(int value){
+        Node current = root;
+        while(current!=null){
+            if(current.value == value){
+                return true;
+            }else if(current.value > value){
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        return false;
     }
 
+    public boolean Insert(int value) {
+        Node newNode = new Node(value);
+
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+
+        Node current = root;
+        Node parent;
+        while(true){
+            parent = current;
+            //Don't want a duplicate value.
+            if (value == current.value){
+                return false;
+            }
+            else if(value < current.value){
+                current = current.left;
+                if(current == null){
+                    parent.left = newNode;
+                    return true;
+                }
+            }else{
+                current = current.right;
+                if(current == null){
+                    parent.right = newNode;
+                    return true;
+                }
+            }
+        }
+    }
 }
